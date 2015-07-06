a3c60f2acad798cdf7f78cc5ce16add8fab552a3
Martijn van Exel <m@rtijn.org>
2015-07-06 09:23:03 -0600

    more help for the constants.

--- shp2maproulette.py
+++ shp2maproulette.py
@@ -8,12 +8,27 @@ import sys
 from fish import ProgressFish
 
 # TODO Constants, these need to be parameters
+
+# This is your source Shapefile
 SHAPEFILE='/Users/martijnv/tmp/crossings/shape/crossings.shp'
+
+# This is the MapRoulette challenge slug
 CHALLENGE_SLUG='fix-railway-crossings'
+
+# And the MapRoulette challenge title
 CHALLENGE_TITLE='Fix U.S. railway crossings'
+
+# The index of the field that will hold your unique identifier
 IDENTIFIER_FIELD=0
+
+# Limit the challenge to this amount of tasks (for debugging purposes)
 LIMIT=100
+
+# The template for the task instruction. Use {} for placeholders to be replaced from attribute values in the Shapefile
 INSTRUCTION_TEMPLATE='The Federal Railway Administration has a record of a level railway crossing at {}. Chances are that OSM does not have a crossing yet. If you see this:\n\n![crossing](https://www.dropbox.com/s/aw5sm6gle51m63f/Screenshot%202015-07-05%2019.16.46.png?dl=1)\n\n OSM already has a crossing and you can skip the task. If the little \'X\' is missing where railway and road intersect, you may need to add a `railway=level_crossing` at the intersection node, and make sure the railway and the road share the node.'
+
+# A list of field indices holding replacement values for the template string above.
+# Make sure the length of this list matches the number of placeholders in your string.
 INSTRUCTION_REPLACEMENT_FIELDS=[4]
 
 # initialize counters
