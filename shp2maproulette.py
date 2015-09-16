94160b4244064d1ec4512fc55ca64b287b77b440
Martijn van Exel <m@rtijn.org>
2015-09-16 15:17:40 -0600

    bugfixes

--- shp2maproulette.py
+++ shp2maproulette.py
@@ -19,7 +19,7 @@ import sys
 import yaml
 from docopt import docopt
 
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 
 # initialize docopt argument parsing
 arguments = docopt(__doc__, version='shp2maproulette {}'.format(__version__))
@@ -29,8 +29,9 @@ options = None
 try:
     with open(arguments['OPTFILE']) as optfile:
         options = yaml.load(optfile)
-except Exception, e:
-    raise e
+except IOError as e:
+    print('The options file cannot be read: {}'.format(e.strerror))
+    sys.exit(1)
 
 if not all (opt in options for opt in (
     'server',
@@ -40,20 +41,21 @@ if not all (opt in options for opt in (
     'identifier_field',
     'instruction_template',
     'instruction_replacement_fields')):
-    print 'options file does not contain all keys.'
+    print 'Options file does not contain all required keys. Refer to the example options file for help.'
     sys.exit(1)
 
 # initialize counter
 row_count = 0
 
 # get a server
-server = MapRouletteServer(options['server'])
+print options.get('server')
+server = MapRouletteServer(**options.get('server'))
 
 # get the challenge
 challenge = None
 challenge = MapRouletteChallenge(
-	slug=options['challenge_slug'],
-	title=options['challenge_title'],
+	slug=options.get('challenge_slug'),
+	title=options.get('challenge_title'),
 	active=True)
 
 # there is no way to upsert a challenge, so we need this conditional
@@ -65,11 +67,11 @@ else:
 	challenge.create(server)
 
 # read in the shapefile features
-shapefile_reader = shapefile.Reader(options['shapefile'])
+shapefile_reader = shapefile.Reader(options.get('shapefile'))
 shapes = shapefile_reader.shapes()
 
 # set limit
-LIMIT = options['limit'] or None
+LIMIT = options.get('limit') or None
 limit = min(LIMIT, len(shapes))
 
 # tasks list
@@ -78,16 +80,16 @@ tasks = []
 # iterate over shapefile features + records
 for shape_record in shapefile_reader.iterShapeRecords():
     task = MapRouletteTask(
-    	shape_record.record[options['identifier_field']],
+    	shape_record.record[options.get('identifier_field')],
         challenge=challenge,
     	geometries=FeatureCollection(
     		[Feature(
     			geometry=shape_record.shape.__geo_interface__)]),
     	instruction=r'{}'.format(
-            options['instruction_template'].format(
+            options.get('instruction_template').format(
                 **{key: shape_record.record[val].strip()
         		for (key, val)
-                in options['instruction_replacement_fields'].items()})))
+                in options.get('instruction_replacement_fields').items()})))
     tasks.append(task)
     row_count = row_count + 1
     if row_count == limit:
